af88bf2e0e4b7e56588ce8c3f819e5bac4328a3a
Vaugtro <victor.agustgm@gmail.com>
2026-02-06 18:07:37 -0300

    chore: cleanup

--- target/debug/deps/core-b851a92ebcc2f899.d
+++
@@ -1,9 +0,0 @@
-C:\Users\wesle\OneDrive\Documents\projetos\voxel-engine\target\debug\deps\core-b851a92ebcc2f899.d: crates\core\src\lib.rs
-
-C:\Users\wesle\OneDrive\Documents\projetos\voxel-engine\target\debug\deps\libcore-b851a92ebcc2f899.rlib: crates\core\src\lib.rs
-
-C:\Users\wesle\OneDrive\Documents\projetos\voxel-engine\target\debug\deps\libcore-b851a92ebcc2f899.rmeta: crates\core\src\lib.rs
-
-crates\core\src\lib.rs:
-
-# env-dep:CARGO_PKG_VERSION=0.1.0
--- target/debug/deps/engine-e40c1e6de7b012a9.d
+++
@@ -1,9 +0,0 @@
-C:\Users\wesle\OneDrive\Documents\projetos\voxel-engine\target\debug\deps\engine-e40c1e6de7b012a9.d: crates\engine\src\lib.rs
-
-C:\Users\wesle\OneDrive\Documents\projetos\voxel-engine\target\debug\deps\libengine-e40c1e6de7b012a9.rlib: crates\engine\src\lib.rs
-
-C:\Users\wesle\OneDrive\Documents\projetos\voxel-engine\target\debug\deps\libengine-e40c1e6de7b012a9.rmeta: crates\engine\src\lib.rs
-
-crates\engine\src\lib.rs:
-
-# env-dep:CARGO_PKG_NAME=engine
